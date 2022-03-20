213b7e576cf83ae2c53a5e669ac611bc7f22decd
dineshigdd <dineshigdd@gmail.com>
2022-03-20 11:40:57 -0700

    add https in index.js in api

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const url = 'http://localhost:5000';
+const url = 'https://localhost:5000';
 
 
 export const fetchBussinessInfo = (search) =>  axios.get(url + `/businesses/search?term=${ search.term }&location=${ search.location }`);
