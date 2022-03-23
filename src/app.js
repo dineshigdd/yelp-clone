a72bae7ddeec8340d15dbb932606f817c7f35e7c
dineshigdd <dineshigdd@gmail.com>
2022-03-23 11:24:12 -0700

    modify path

--- src/app.js
+++ src/app.js
@@ -33,7 +33,7 @@ app.use('/businesses', businessesRoutes );
 
 if( process.env.NODE_ENV == 'production'){
     app.use( express.static('../frontend/build'));
-    app.get('/', ( req, res ) =>{
+    app.get('*', ( req, res ) =>{
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
     })
 }
