96c9601f45c15bc2714ac8f2522800f22e06a5ac
dineshigdd <dineshigdd@gmail.com>
2022-03-27 08:34:33 -0700

    change git pocfile place

--- src/app.js
+++ src/app.js
@@ -38,6 +38,6 @@ if( process.env.NODE_ENV == 'production'){
     })
 }
 
-module.exports = app;
-// const port = process.env.PORT || 5000;
-// app.listen(port, () => console.log(`Server is running on port ${port}`));
+// module.exports = app;
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
