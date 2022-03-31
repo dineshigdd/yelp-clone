090b162ab3e5574c05be77c64d874111937ae221
dineshigdd <dineshigdd@gmail.com>
2022-03-30 20:55:36 -0700

    add server url to api/index.js in client

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 
 
+const url= "https://yelp-clone-server.herokuapp.com";
 
+export const fetchBussinessInfo = (search) =>  axios.get(`${ url }/businesses/search?term=${ search.term }&location=${ search.location }`);
 
-export const fetchBussinessInfo = (search) =>  axios.get(`/businesses/search?term=${ search.term }&location=${ search.location }`);
-
-export const fetchReviewInfo = ( id ) =>  axios.get( `/businesses/${ id }`);
+export const fetchReviewInfo = ( id ) =>  axios.get( `${ url }/businesses/${ id }`);
 
